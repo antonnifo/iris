0c33aaba74227fcc7fbf261604469d331b8a0573
antonnifo <antonnifo@live.com>
2019-10-23 19:57:47 +0300

    set up api

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from flask_restful import Api
+from iris import IrisClassifier
+
+# created an object of flask using a unique name
+app = Flask(__name__)
+app.config['PROPAGATE_EXCEPTIONS'] = True 
+api = Api(app)
+
+
+api.add_resource(IrisClassifier ,'/iris')        
+      
+
+if __name__ == '__main__':
+    app.run()
